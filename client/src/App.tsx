cbb2a688662ba645a3d961e928cd58d558e45fe1
unknown <verwiel.code@gmail.com>
2020-12-08 10:24:29 -0700

    set up base style structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import logo from './assets/logo.svg'
+import { Counter } from './features/counter/Counter'
 
 function App() {
   return (
@@ -55,4 +54,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
